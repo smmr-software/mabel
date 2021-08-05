faa03dd2f8e8e0b39f6e587b757fe0c43c9752a5
FIGBERT <figbert@figbert.com>
2021-08-04 21:52:27 -0700

    Combine variable declarations in styles.go

--- styles.go
+++ styles.go
@@ -2,20 +2,22 @@ package main
 
 import gloss "github.com/charmbracelet/lipgloss"
 
-var border = gloss.RoundedBorder()
-var borderWindow = gloss.NewStyle().
-	Align(gloss.Center).
-	BorderStyle(border).
-	BorderForeground(gloss.Color("86"))
-var title = gloss.NewStyle().
-	Bold(true).
-	Underline(true).
-	Foreground(gloss.Color("#FFF"))
+var (
+	border       = gloss.RoundedBorder()
+	borderWindow = gloss.NewStyle().
+			Align(gloss.Center).
+			BorderStyle(border).
+			BorderForeground(gloss.Color("86"))
+	title = gloss.NewStyle().
+		Bold(true).
+		Underline(true).
+		Foreground(gloss.Color("#FFF"))
 
-var button = struct {
-	inactive gloss.Style
-	active   gloss.Style
-}{
-	inactive: gloss.NewStyle().Foreground(gloss.Color("#000")).Background(gloss.Color("#FFF")),
-	active:   gloss.NewStyle().Foreground(gloss.Color("#000")).Background(gloss.Color("86")),
-}
+	button = struct {
+		inactive gloss.Style
+		active   gloss.Style
+	}{
+		inactive: gloss.NewStyle().Foreground(gloss.Color("#000")).Background(gloss.Color("#FFF")),
+		active:   gloss.NewStyle().Foreground(gloss.Color("#000")).Background(gloss.Color("86")),
+	}
+)
