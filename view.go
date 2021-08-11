eed172aea2d4276d4f66a7b4c8acfe5f8222a55b
FIGBERT <figbert@figbert.com>
2021-08-10 17:20:31 -0700

    Speed up View() by switching to strings.Builder

--- view.go
+++ view.go
@@ -1,6 +1,9 @@
 package main
 
-import gloss "github.com/charmbracelet/lipgloss"
+import (
+	gloss "github.com/charmbracelet/lipgloss"
+	"strings"
+)
 
 func (m model) View() string {
 	fullscreen := gloss.NewStyle().
@@ -16,41 +19,42 @@ func (m model) View() string {
 		Width(int(float64(m.width)*0.9)).
 		Border(gloss.NormalBorder(), false, false, true)
 
-	var body string
-	if torrents := m.torrent.client.Torrents(); len(torrents) > 0 {
-		body += entry.Render("You have some torrents!")
-	} else {
-		body += entry.Render("You have no torrents!")
-	}
-	body += "\n"
+	var body strings.Builder
 
 	if m.addPrompt.enabled {
-		body := title.Render("Add Torrent") + "\n\n"
+		body.WriteString(title.Render("Add Torrent") + "\n\n")
 		if m.addPrompt.state.download == "magnet" {
-			body += button.active.Render(" Magnet ") + "    "
-			body += button.inactive.Render(" Torrent ") + "\n\n\n"
-			body += m.addPrompt.state.magnetLink.View() + "\n\n"
+			body.WriteString(button.active.Render(" Magnet ") + "    ")
+			body.WriteString(button.inactive.Render(" Torrent ") + "\n\n\n")
+			body.WriteString(m.addPrompt.state.magnetLink.View() + "\n\n")
 		} else {
-			body += button.inactive.Render(" Magnet ") + "    "
-			body += button.active.Render(" Torrent ") + "\n\n\n"
-			body += m.addPrompt.state.torrentPath.View() + "\n\n"
+			body.WriteString(button.inactive.Render(" Magnet ") + "    ")
+			body.WriteString(button.active.Render(" Torrent ") + "\n\n\n")
+			body.WriteString(m.addPrompt.state.torrentPath.View() + "\n\n")
 		}
-		body += m.addPrompt.state.savePath.View() + "\n\n\n\n"
+		body.WriteString(m.addPrompt.state.savePath.View() + "\n\n\n\n")
 		if m.addPrompt.phase == "approval" {
-			body += button.active.Render(" Start Download ")
+			body.WriteString(button.active.Render(" Start Download "))
 		} else {
-			body += button.inactive.Render(" Start Download ")
+			body.WriteString(button.inactive.Render(" Start Download "))
 		}
 		return fullscreen.Render(
 			gloss.Place(
 				m.width, m.height,
 				gloss.Center, gloss.Center,
-				popup.Render(body),
+				popup.Render(body.String()),
 				gloss.WithWhitespaceChars("⑀"),
 				gloss.WithWhitespaceForeground(gloss.Color("#383838")),
 			),
 		)
+	} else {
+		if torrents := m.torrent.client.Torrents(); len(torrents) > 0 {
+			body.WriteString(entry.Render("You have some torrents!"))
+		} else {
+			body.WriteString(entry.Render("You have no torrents!"))
+		}
+		body.WriteString("\n")
 	}
 
-	return fullscreen.Render(body)
+	return fullscreen.Render(body.String())
 }
