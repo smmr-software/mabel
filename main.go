9b88d233ad7e363571dd9b9bce695e5549c59f4a
FIGBERT <figbert@figbert.com>
2022-04-16 00:30:52 -0700

    go fmt main.go

--- main.go
+++ main.go
@@ -89,7 +89,7 @@ func main() {
 		realTheme = styles.DesertTheme
 	}
 	styles.BorderWindow = styles.BorderWindow.BorderForeground(realTheme.Primary)
-	
+
 	if flag.NArg() == 1 {
 		mini.Execute(&args[0], download, port, &realTheme)
 	} else {
