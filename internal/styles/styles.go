5973ace8ab31863418095b26631ede276968349e
art3mis <moon@art3mis.pm>
2022-05-24 12:13:45 -0700

    Add comments to styles package

--- internal/styles/styles.go
+++ internal/styles/styles.go
@@ -1,7 +1,10 @@
+// Package styles defines the color themes and Lip Gloss styles for
+// Mabel based on the user config file.
 package styles
 
 import gloss "github.com/charmbracelet/lipgloss"
 
+// Define the Lip Gloss styles for borders and bold text
 var (
 	Bold = gloss.NewStyle().Bold(true)
 
