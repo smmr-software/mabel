b7bbe5bed1682314f7f1a71bb48880f9b24e8781
art3mis <moon@art3mis.pm>
2022-06-26 15:57:30 -0700

    Create custom key map struct to change bindings

--- full/keys.go
+++ full/keys.go
@@ -2,6 +2,26 @@ package full
 
 import "github.com/charmbracelet/bubbles/key"
 
+type CustomKeyMap struct {
+	customHomeKeyMap struct {
+		up       string
+		down     string
+		next     string
+		prev     string
+		add      string
+		delete   string
+		details  string
+		deselect string
+		help     string
+		quit     string
+	}
+	customAddPromptKeyMap struct {
+		quit    string
+		back    string
+		forward string
+	}
+}
+
 type homeKeyMap struct {
 	up       key.Binding
 	down     key.Binding
@@ -115,4 +135,85 @@ var addPromptKeys = addPromptKeyMap{
 		key.WithKeys("enter", "tab"),
 		key.WithHelp("↵", "next"),
 	),
+}
+
+func (c *CustomKeyMap) ToKeys() {
+	if c.customHomeKeyMap.up != "" {
+		homeKeys.up = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.up),
+			key.WithHelp(c.customHomeKeyMap.up, "up"),
+		)
+	}
+	if c.customHomeKeyMap.down != "" {
+		homeKeys.down = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.down),
+			key.WithHelp(c.customHomeKeyMap.down, "down"),
+		)
+	}
+	if c.customHomeKeyMap.next != "" {
+		homeKeys.next = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.next),
+			key.WithHelp(c.customHomeKeyMap.next, "next page"),
+		)
+	}
+	if c.customHomeKeyMap.prev != "" {
+		homeKeys.prev = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.prev),
+			key.WithHelp(c.customHomeKeyMap.prev, "prev page"),
+		)
+	}
+	if c.customHomeKeyMap.add != "" {
+		homeKeys.add = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.add),
+			key.WithHelp(c.customHomeKeyMap.add, "add torrent"),
+		)
+	}
+	if c.customHomeKeyMap.delete != "" {
+		homeKeys.delete = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.delete),
+			key.WithHelp(c.customHomeKeyMap.delete, "delete"),
+		)
+	}
+	if c.customHomeKeyMap.deselect != "" {
+		homeKeys.deselect = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.deselect),
+			key.WithHelp(c.customHomeKeyMap.deselect, "deselect"),
+		)
+	}
+	if c.customHomeKeyMap.details != "" {
+		homeKeys.details = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.details),
+			key.WithHelp(c.customHomeKeyMap.details, "torrent details"),
+		)
+	}
+	if c.customHomeKeyMap.help != "" {
+		homeKeys.help = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.help),
+			key.WithHelp(c.customHomeKeyMap.help, "toggle help"),
+		)
+	}
+	if c.customHomeKeyMap.quit != "" {
+		homeKeys.quit = key.NewBinding(
+			key.WithKeys(c.customHomeKeyMap.quit),
+			key.WithHelp(c.customHomeKeyMap.quit, "quit"),
+		)
+	}
+	if c.customAddPromptKeyMap.quit != "" {
+		addPromptKeys.quit = key.NewBinding(
+			key.WithKeys(c.customAddPromptKeyMap.quit),
+			key.WithHelp(c.customAddPromptKeyMap.quit, "home"),
+		)
+	}
+	if c.customAddPromptKeyMap.back != "" {
+		addPromptKeys.back = key.NewBinding(
+			key.WithKeys(c.customAddPromptKeyMap.back),
+			key.WithHelp(c.customAddPromptKeyMap.back, "home"),
+		)
+	}
+	if c.customAddPromptKeyMap.forward != "" {
+		addPromptKeys.forward = key.NewBinding(
+			key.WithKeys(c.customAddPromptKeyMap.forward),
+			key.WithHelp(c.customAddPromptKeyMap.forward, "home"),
+		)
+	}
 }
